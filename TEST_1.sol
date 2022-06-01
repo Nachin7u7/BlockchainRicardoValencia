d57aa8a28e1fa8727cc5f0a5d733c09c05c4628c
unknown <ignacio.valencia.sauma@gmail.com>
2022-06-01 13:31:13 -0400

    funciones restantes

--- TEST_1.sol
+++ TEST_1.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
+// Ricardo Ignacio Valencia Sauma
+// Codigo: 51231
+// Examen B
+
 contract TEST_1 {
     address private owner;
     bool private isClosed = false;
@@ -16,41 +20,61 @@ contract TEST_1 {
         owner = msg.sender;
     }
 
-    mapping(uint256 => Account) public listAccounts;
+    mapping(uint256 => Account) public accounts;
 
-    function createAccount(Account memory accountData)
+    function createAccount(Account memory data)
         public
         onlyOwner(msg.sender)
         closedBank
     {
         require(
-            bytes(accountData.name).length > 5,
+            bytes(data.name).length > 5,
             "Nombre muy corto, el nombre debe ser mayor a 5"
         );
 
-        listAccounts[accountData.id] = Account(
-            true,
-            accountData.id,
-            0,
-            accountData.name
-        );
+        accounts[data.id] = Account(true, data.id, data.name, 0);
     }
 
     function deposit(uint256 id) public payable closedBank {
-        Account memory data = listAccounts[id];
+        Account memory data = accounts[id];
         uint256 mount = convertWeiToEther(msg.value);
-        listAccounts[id].balance += mount;
+        accounts[id].balance += mount;
         require(!data.enable, "Cuenta no valida, su cuenta esta desactivada");
         require(mount > 2, "El deposito minimo es de 2 ETH");
-        emit clientDeposit(data.id, data.name, mount);
+        emit clientDeposit(data.id, mount, data.name);
         if (mount > 10) payable(msg.sender).transfer(1);
     }
 
     function redrawMoney(uint256 id, uint256 mount) public payable closedBank {
-        Account memory data = listAccounts[id];
+        Account memory data = accounts[id];
         require(!data.enable, "Cuenta no valida, su cuenta esta desactivada");
         require(mount <= data.balance, "No tiene suficiente dinero");
-        listAccounts[id].balance -= mount;
+        accounts[id].balance -= mount;
         payable(msg.sender).transfer(mount);
+    }
+
+    function openCloseBank(bool value) public onlyOwner(msg.sender) {
+        isClosed = value;
+    }
+
+    function blockAccount(uint256 id) public onlyOwner(msg.sender) closedBank {
+        accounts[id].enable = false;
+    }
+
+    function withdrawAllMoney() public onlyOwner(msg.sender) {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function convertWeiToEther(uint256 amount) private pure returns (uint256) {
+        return amount / 1 ether;
+    }
+
+    modifier onlyOwner(address wallet) {
+        require(owner == wallet, "Usted no es el owner");
+        _;
+    }
+    modifier closedBank() {
+        require(!isClosed, "El banco esta cerrado");
+        _;
     }
 }
