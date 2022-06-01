7cffc531d879fbfdb48517493428d993cd7ce05c
unknown <ignacio.valencia.sauma@gmail.com>
2022-06-01 13:01:12 -0400

    pregunta 1

---
+++ TEST_1.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract TEST_1 {
+    address private owner;
+    bool private isClosed = false;
+    event clientDeposit(uint256 id, uint256 amount, string name);
+    struct Account {
+        bool enable;
+        uint256 id;
+        string name;
+        uint256 balance;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    mapping(uint256 => Account) public listAccounts;
+
+    function createAccount(Account memory accountData)
+        public
+        onlyOwner(msg.sender)
+        closedBank
+    {
+        require(
+            bytes(accountData.name).length > 5,
+            "the name of product should be more than 5."
+        );
+        listAccounts[accountData.id] = Account(
+            accountData.id,
+            accountData.name,
+            0,
+            true
+        );
+    }
+}
