4d5c8f8f9559e5e774eb0d4308fae081ebb5dc9c
unknown <ignacio.valencia.sauma@gmail.com>
2022-06-01 13:15:29 -0400

    pregunta 2 & 3

--- TEST_1.sol
+++ TEST_1.sol
@@ -27,15 +27,30 @@ contract TEST_1 {
             bytes(accountData.name).length > 5,
             "Nombre muy corto, el nombre debe ser mayor a 5"
         );
-        
+
         listAccounts[accountData.id] = Account(
             true,
             accountData.id,
             0,
             accountData.name
-            
         );
     }
 
+    function deposit(uint256 id) public payable closedBank {
+        Account memory data = listAccounts[id];
+        uint256 mount = convertWeiToEther(msg.value);
+        listAccounts[id].balance += mount;
+        require(!data.enable, "Cuenta no valida, su cuenta esta desactivada");
+        require(mount > 2, "El deposito minimo es de 2 ETH");
+        emit clientDeposit(data.id, data.name, mount);
+        if (mount > 10) payable(msg.sender).transfer(1);
+    }
 
+    function redrawMoney(uint256 id, uint256 mount) public payable closedBank {
+        Account memory data = listAccounts[id];
+        require(!data.enable, "Cuenta no valida, su cuenta esta desactivada");
+        require(mount <= data.balance, "No tiene suficiente dinero");
+        listAccounts[id].balance -= mount;
+        payable(msg.sender).transfer(mount);
+    }
 }
