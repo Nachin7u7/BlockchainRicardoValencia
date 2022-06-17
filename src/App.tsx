07528cecff0da085d5d6798c5ae68484f7317b68
unknown <ignacio.valencia.sauma@gmail.com>
2022-06-17 03:43:06 -0400

    10-06

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { connectWallet, initialize } from './ethereum/web3';
+
+function App() {
+  /*
+  componentDidMount(){
+  }
+   */
+  useEffect(() => {
+    // @ts-ignore
+    if(window.web3){
+      initialize();
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>Hi React, Truffle, Firebase</p>
+        <button onClick={()=> connectWallet()}>Connect</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
